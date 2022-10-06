4d7b6a26d7614414c7b17e39eceaeb8d0ba5b5b1
andre-fajar-n <andrenugroho395@gmail.com>
2022-10-06 09:29:15 +0700

    create endpoint check harga

---
+++ microservice/cek-harga-service/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"jojonomic/utils"
+	"jojonomic/utils/model"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	utils.InitConfig()
+
+	utils.InitializeDatabase()
+
+	r.HandleFunc("/api/check-harga", handlerCheckHarga).Methods("GET")
+
+	log.Fatal(http.ListenAndServe("localhost:8004", r))
+}
+
+func handlerCheckHarga(w http.ResponseWriter, r *http.Request) {
+	var data model.TblHarga
+
+	err := utils.DB.Model(&data).First(&data).Order("created_at DESC").Error
+	if err != nil {
+		utils.WriteErrorResponse(w, "", err)
+		return
+	}
+
+	utils.WriteSuccessResponseWithData(w, map[string]interface{}{
+		"harga_buyback": data.HargaBuyback,
+		"harga_topup":   data.HargaTopup,
+	}, "")
+}
